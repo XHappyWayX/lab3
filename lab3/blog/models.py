43992c32b3636dd0eed8d6c4eb5cae6418c6d58e
nikita <hhappywayy@gmail.com>
2023-10-17 02:05:24 +0300

    fin

--- lab3/blog/models.py
+++ lab3/blog/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=255, default='')
     content = models.TextField()
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField()
     category = models.CharField(max_length=255, default='')
 
 class Comment(models.Model):
-    post = models.ForeingKey(Post)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.CharField(max_length=100, default='')
     content = models.TextField()
     date = models.DateTimeField()
