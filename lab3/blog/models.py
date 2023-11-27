41b6ca2e8929bbbdef9d1bd93c55ab2b39c05a0e
nikita <hhappywayy@gmail.com>
2023-11-27 15:13:46 +0200

    done

--- lab3/blog/models.py
+++ lab3/blog/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255, default='')
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField()
+    date = models.DateField()
     category = models.CharField(max_length=255, default='')
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.CharField(max_length=100, default='')
     content = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateField()
